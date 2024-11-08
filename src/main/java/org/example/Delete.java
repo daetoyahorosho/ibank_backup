08a04fb164608b9dc7c6219b26d356871972f2d4
mastersborok <chishik1000000@gmail.com>
2024-11-08 11:00:55 +0300

    Эксперимент с MessageFormat:
    - реализация SQL-запросов в приватных констатнтах;
    - использование MessageFormat в формировании SQL-запроса.

--- src/main/java/org/example/Delete.java
+++ src/main/java/org/example/Delete.java
@@ -1,16 +1,21 @@
 package org.example;
 
 import java.sql.*;
+import java.text.MessageFormat;
+import java.util.Objects;
 
 public class Delete {
 
-    // Приветаный конструктор
+    private static String CHECK_SCHEMA_QUERY = "SELECT schema_name FROM information_schema.schemata WHERE schema_name = ''{0}''";
+    private static String DROP_CHEMA = "DROP SCHEMA IF EXISTS {0} CASCADE";
+
+    // Приватный конструктор
     private Delete() {
     }
 
     static void deleteSchema() throws SQLException {
 
-        Config config = Config.getInstance(null);
+        Config config = Config.getInstance("src/main/resources/config.properties");
 
         try (Connection conn = DriverManager.getConnection(config.getDbUrl(), config.getDbUser(), config.getDbPassword());
              Statement stmt = conn.createStatement()) {
@@ -18,21 +23,22 @@ public class Delete {
             System.out.println("Подключение установлено!");
 
             // Проверка, существует ли схема
-            String checkSchemaExists = "SELECT schema_name FROM information_schema.schemata WHERE schema_name = '" + config.getDbSchema() + "'";
+            String checkSchemaExists = MessageFormat.format(CHECK_SCHEMA_QUERY, config.getDbSchema());
             ResultSet resultSet = stmt.executeQuery(checkSchemaExists);
 
             // Переходим к первой строке результата
             if (resultSet.next()) {
-                String schemaName = resultSet.getString("schema_name");
-                System.out.println("Schema name from database: " + schemaName);
-
                 // Схема существует, удаляем ее
-                String deleteSchemeCascade = "DROP SCHEMA IF EXISTS " + config.getDbSchema() + " CASCADE";
+                String deleteSchemeCascade = MessageFormat.format(DROP_CHEMA, config.getDbSchema());
                 stmt.executeUpdate(deleteSchemeCascade);
                 System.out.println("Схема " + config.getDbSchema() + " успешно удалена каскадно.");
             } else {
                 System.out.println("Схема " + config.getDbSchema() + " отсутствует");
             }
         }
+    }
+
+    public static void main(String[] args) throws SQLException {
+        deleteSchema();
     }
 }
