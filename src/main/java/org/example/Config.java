7c35c4b2c6027ef6f0680a73274ca0fe71ea5d56
mastersborok <chishik1000000@gmail.com>
2024-11-08 19:27:54 +0300

    Улучшение читаемости кода:
    - приведение к единообразию название переменных из config.properties

--- src/main/java/org/example/Config.java
+++ src/main/java/org/example/Config.java
@@ -37,27 +37,28 @@ class Config {
         return properties.getProperty("db.password");
     }
 
+    public String getDbName() {
+        return properties.getProperty("db.name");
+    }
+
     public String getDbSchema() {
         return properties.getProperty("db.schema");
     }
 
     public String getPathPgRestore() {
-        return properties.getProperty("pathPgRestore");
+        return properties.getProperty("path.restore");
     }
 
     public String getDumpFileDb() {
-        return properties.getProperty("dumpFileDb");
+        return properties.getProperty("path.filedb");
     }
 
     public String getIbankFolder() {
-        return properties.getProperty("ibankFolder");
+        return properties.getProperty("path.backup");
     }
 
     public String getBackupIbankFolder() {
-        return properties.getProperty("backupIbankFolder");
+        return properties.getProperty("path.ibank");
     }
 
-    public String getDbName() {
-        return properties.getProperty("db.name");
-    }
 }
